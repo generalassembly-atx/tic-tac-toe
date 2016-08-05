959c9d523de0c53d3c8ebab7d227b1ab47569a90
Jeanette Adelson <jeni.jeanette@gmail.com>
2016-08-05 14:23:45 -0500

    is cell empty

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
+$(document).ready(function () {
+  $('.squares').on('click', function () {
+    var marker = ('x' || 'o')
+    if ($('.squares').text() === marker) {
+      alert('This square is already taken! Choose another.')
+    } else {
+        if ($(this) === $('.playerOne') {
+          $('.squares').text('x')
+        } else {
+        } $('.squares').text('o')
+    }
+  })
 
+});
+
+//need to call it still
+function moveMaker() {
+  if ($('#currentPlayer').text() === '1') {
+    $('#currentPlayer').text('2')
+  } else {
+    $('#currentPlayer').text('1')
+  }
+}
