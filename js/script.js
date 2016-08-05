755a2c3565c833fd5fed08142b41568d0011a699
Richard Davis <rddavistx@hotmail.com>
2016-08-05 16:56:24 -0500

    first TTT commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
+$(document).ready(function() {
 
+var count = 0;
+        $('td').on('click', function(){
+            if (count % 2 === 0) {
+              $(this).text('X');
+              $('span').text("1");
+              checkForWin('X');
+            } else {
+           //player 2's turn (O)
+              $(this).text('O');
+              $('span').text('2');
+              checkForWin('O');
+            }
+          count++;
+
+      });
+
+
+      function checkForWin() {
+          if ($('#1').text() === $('#2').text() && $('#2').text() === $('#3').text()) {
+            console.log('win');
+          }
+
+
+
+      }
+
+
+
+
+
+});
