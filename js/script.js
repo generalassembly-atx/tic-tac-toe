ac47001c3f3354bc8244cd7d1b80888721a96b6c
Aaron Baker <aaronbaker9210@gmail.com>
2016-10-02 10:22:05 -0500

    10:22am

--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@ $(document).ready(function() {
 
   // Global Variables
   var player = 1;
+  var playerOneScore = 0;
+  var playerTwoScore = 0;
   var turnsTaken = 0;
   var squaresList = $('.square');
 
 // setup event listeners on all the squares
-
   for (var i = 0; i < squaresList.length; i++) {
     $(squaresList[i]).on('click', squareClick)
   }
@@ -60,55 +61,91 @@ $(document).ready(function() {
   lines.push(diag1);
   lines.push(diag2);
 
-
+// ****
+// The data structure is important because it contains the winning combinations
+// For example, It contains a list called row1. It contains another list called column3. column2. diag1.
+// These lists are containers, holding the individual cells
+// This is convenient because we can loop through the elements of these lists and test for a win each time.
+// (1) look into row1, look at each cell, look at the value of each cell. is this a winning combo?
+// (2) look into row2, look at each cell, look at the value of each cell. is this a winning combo?
+// (3) look into row 3,
+// (4) look into column 1
+// (..) and so on....
+// ****
 
 
 
   // Function Definitions
   function squareClick(event) {
+
+    // A cell that already contains a value has been clicked
+    // ** illegal move **
+
     if ($(this).html() != '') {
       alert('choose a better spot');
 
+    // places an X and tests for a Win
     } else if (player === 1) {
       $(this).html('<p>X</p>').hide().fadeIn(600);
       $(this).data('character', 'x');
       testWin();
+      // sets next player
       player = 2;
       $('h2').find('span').text('2');
     }
 
+
+    // places an O and tests for a Win
     else if (player === 2) {
       $(this).html('<p>O</p>').hide().fadeIn(600);;
       $(this).data('character', 'o'); // "oh" not "zero"
       testWin();
+      // sets next player
       player = 1;
       $('h2').find('span').text('1');
     }
 
   };
 
+
+
+  // **********
+  // tests for a winner
+  // this function is run every time a click is placed
+  // *************************************************
+
   function testWin() {
+
+    // keeping a tally on how many turns have been taken (when == 9, game over)
     turnsTaken += 1;
+    //
 
+    // looking into the data structure
+    //
+    // ** this FOR-IN LOOP looks strange but it works.
+    // ** lines is a container that holds all the possible 3-element lines on the board
+    // ** for example, row 1 is a line. column 1 is a line. diagonal 2 is a line.
+    // **
     for (var lineIndex in lines) {
-
-
-
       var line = lines[lineIndex];
 
-
-
-
       var test = "";
+      // Look into each line that we retrieve from lines.
+      // Get it's data-character (none, x, o)
+      // Append it to test string
       for (var i=0; i<3 ;i++){
         test+=$(line[i]).data('character');
       }
+
+      // content of test string gives the answer. is there a winner? is there a loser?
       if (test == 'xxx') {
         xWins(line);
       } else if (test == "ooo") {
         oWins(line);
-      } 
+      }
     }
+
+    // if all the spots have been clicked and there is still no 'xxx' or 'ooo', then there is a tie.
     if (turnsTaken===9) {
       catWins();
     }
@@ -119,27 +156,45 @@ $(document).ready(function() {
   };
 
   function xWins() {
-    $('.board').fadeOut(500);
-    $('body').append('<h1 class="winner">Player 1 wins!</h1>');
+    hideBoard();
+    $('.win-log').append('<h1 class="winner">Player 1 wins!</h1>');
+    playerOneScore += 1;
+    $('#playerOneScore').text(playerOneScore);
 
   }
   function oWins() {
-  $('.board').fadeOut(1000);
-  $('body').append('<h1 class="winner">Player 2 wins!</h1>');
+  hideBoard();
+  $('.win-log').append('<h1 class="winner">Player 2 wins!</h1>');
+  playerTwoScore += 1;
+  $('#playerTwoScore').text(playerTwoScore);
 
   }
 
   function catWins() {
-    $('.board').fadeOut(500);
+    // $('.board').fadeOut(500);
     $('body').append('<h1 class="winner">Cat wins!</h1>');
+  }
+  function hideBoard() {
+    console.log('hiding board');
+    board = $('.board');
+    board.hide();
+    // board.css ('position', 'relative');
+  //   board.animate({'opacity': '0',
+  //                        'right': '300px',
+  //
+  // }, 900);
+
   }
 
   function reset() {
     console.log('resetting!');
-    // $('.board').empty();
-    // $('.board').initialize();
-
+    $('.win-log').empty();
+    $('.board').show();
     // for every cell in the board, wipe its data and its html content
+    for (var i = 0; i < squaresList.length; i++) {
+      $(squaresList[i]).html('');
+      $(squaresList[i]).data('character', 'none');
+    }
 
   }
 
