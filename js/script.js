47e24af7f2d3319b944ce8e00f3e39482f75d63a
Clark Sanford <clrksanford@gmail.com>
2016-11-10 17:47:05 -0600

    Uploading to github for inclusion in portfolio

--- js/script.js
+++ js/script.js
@@ -9,15 +9,30 @@ function makeAMove(e) {
   var cell = $(e.currentTarget);
 
 //Check for marked square
-
   if(cell.text() !== "") {
-    alert('That space is already taken!');
+    showFullMessage();
+    window.setTimeout(removeFullMessage,1500);
     return false;
   }
 
 //Place 'x' or 'o' depending on which player is current
-
   checkPlayer(cell);
+}
+
+function showFullMessage() {
+  $('#cellFull').css('display','block');
+}
+
+function removeFullMessage() {
+  $('#cellFull').css('display','none');
+}
+
+function showWinMessage() {
+  $('#winner').css('display','block');
+}
+
+function showDrawMessage() {
+  $('#draw').css('display','block');
 }
 
 function checkPlayer(element) {
@@ -29,7 +44,6 @@ function checkPlayer(element) {
     $('#current span').text('2');
   } else {
     element.text('O');
-    element.addClass('O');
     window.setTimeout(checkForMatch,1000);
     $('#current span').text('1');
   }
@@ -53,23 +67,25 @@ function checkForMatch() {
   var playerTwoWins = row1 === "OOO" || row2 === "OOO" || row3 === "OOO" || rDiagonal === "OOO" || lDiagonal === "OOO" || col1 === "OOO" || col2 === "OOO" || col3 === "OOO";
 
   if (playerOneWins) {
-    alert("Congratulations, Player 1! You won!");
+    showWinMessage();
     playerOneScore ++;
     $('#playerOneScore').text(playerOneScore);
-    resetBoard();
+    window.setTimeout(resetBoard,1500);
   } else if (playerTwoWins) {
-    alert("Congratulations, Player 2! You won!");
+    showWinMessage();
     playerTwoScore ++;
     $('#playerTwoScore').text(playerTwoScore);
-    resetBoard();
+    window.setTimeout(resetBoard,1500);
   } else if ((row1 + row2 + row3).length === 9) {
-    alert("It's a draw!");
-    resetBoard();
+    showDrawMessage();
+    window.setTimeout(resetBoard,1500);
   }
 }
 
 function resetBoard() {
   $('.board').find('td').text('');
+  $('#winner').css('display','none');
+  $('#draw').css('display','none');
 }
 
 function restart() {
